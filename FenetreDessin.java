bae920a14ac34bda7a2ed428e15091b1180c3abc
Sofiane FLIH <flih.sofiane@gmail.com>
2012-03-27 12:48:35 +0200

    compatibilite lecture fichier/affichage

--- FenetreDessin.java
+++ FenetreDessin.java
@@ -11,13 +11,13 @@ public class FenetreDessin extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-    protected GraphicsEnvironment graphicsEnv=GraphicsEnvironment.getLocalGraphicsEnvironment();//get local graphics environment	
-    protected Rectangle maxWindowBounds=graphicsEnv.getMaximumWindowBounds();//get maximum window bounds
-    protected int hauteur = (int)maxWindowBounds.getHeight();
-    protected int largeur = (int)maxWindowBounds.getWidth();
-    JPanelDessin panel;
-    boolean [][] obstaclesTab;
-    
+	protected GraphicsEnvironment graphicsEnv=GraphicsEnvironment.getLocalGraphicsEnvironment();//get local graphics environment	
+	protected Rectangle maxWindowBounds=graphicsEnv.getMaximumWindowBounds();//get maximum window bounds
+	protected int hauteur = (int)maxWindowBounds.getHeight();
+	protected int largeur = (int)maxWindowBounds.getWidth();
+	JPanelDessin panel;
+	boolean [][] obstaclesTab;
+
 	public FenetreDessin(boolean [][] tab){
 		obstaclesTab = tab;
 		super.setTitle("Projet Long");
@@ -26,25 +26,10 @@ public class FenetreDessin extends JFrame{
 		super.setBackground(Color.WHITE);
 		super.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
-	
+
 	public void construit(){
 		panel = new JPanelDessin(obstaclesTab);
 		super.setContentPane(panel);
 		super.setVisible(true);
-	}
-	public static void main(String[]args){
-		boolean [][] tabtemp = new boolean [15][8];
-		for(int i=0;i<tabtemp.length;i++){
-			for(int j=0; j<tabtemp[0].length;j++){
-				tabtemp[i][j]=false;
-			}
-		}
-		tabtemp[10][3]=true;
-		tabtemp[10][4]=true;
-		tabtemp[9][3]=true;
-		tabtemp[9][2]=true;
-		tabtemp[0][0]=true;
-		FenetreDessin fd = new FenetreDessin(tabtemp);
-		fd.construit();
 	}
 }
