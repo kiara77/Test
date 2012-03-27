c17dfe5cf8c901cdca55f6b8250a1b537791087e
Sofiane FLIH <flih.sofiane@gmail.com>
2012-03-27 12:57:36 +0200

    compatibilite lecture/affichage fix

--- Main.java
+++ Main.java
@@ -4,13 +4,7 @@ public class Main{
 
 	public static void main(String[] args) throws FileNotFoundException{
 		BoolArray b = new BoolArray("boolean.txt");
-		int sizeX = b.getSizeX();
-		int sizeY = b.getSizeY();
-		for(int i=0; i<sizeX; i++){
-			for(int j=0; j<sizeY; j++){
-				System.out.print((b.getCell(i,j))==true ? 1 : 0);
-			}
-			System.out.println();
-		}
+		FenetreDessin fd = new FenetreDessin(b.getGrid());
+		fd.construit();
 	}
 }
